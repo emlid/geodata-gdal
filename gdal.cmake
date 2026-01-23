b7e98c28a18057fd71937e4d46b9bd1374f725b6
Ivan Zakharov <ivan.zakharov@emlid.com>
2026-01-23 19:07:32 +0000

    disable uninstall target

--- gdal.cmake
+++ gdal.cmake
@@ -656,14 +656,18 @@ if (NOT GDAL_ENABLE_MACOSX_FRAMEWORK)
     COMPONENT libraries)
 endif ()
 
-if(NOT TARGET uninstall)
-    configure_file(
-        "${PROJECT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
-        IMMEDIATE @ONLY)
+if(PROJECT_IS_TOP_LEVEL)
+    if(NOT TARGET uninstall)
+        configure_file(
+            "${PROJECT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+            "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
+            IMMEDIATE @ONLY)
 
-    add_custom_target(uninstall
-        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
+        add_custom_target(uninstall
+            COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
+    endif()
+else()
+    message(STATUS "GDAL: Building as a sub-project, skipping 'uninstall' target and template configuration.")
 endif()
 
 ################################################################
